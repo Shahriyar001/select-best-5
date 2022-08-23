074ab6663d14329ff17bf9f6f076bdd869083a62
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-08-23 15:27:33 +0600

    add js files

---
+++ js/selaction.js
@@ -0,0 +1,8 @@
+function addToSelacted(element) {
+    // console.log(element.parentNode.parentNode.children);
+    // console.log(element.parentNode.parentNode.children[0].innerText);
+    // console.log(element.parentNode.children[0]);
+
+    const playerName = element.parentNode.parentNode.children[0].innerText;
+    console.log(playerName);
+}
