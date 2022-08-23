49ecc37c4bee71846b33f313c71aff3fcda7885b
Shahriyar001 <shahriyarmahbub55@gmail.com>
2022-08-23 21:09:11 +0600

    calculate functions

--- js/selaction.js
+++ js/selaction.js
@@ -30,7 +30,7 @@ function addToSelacted(element) {
     // console.log(playerName);
 
     const playerObj = {
-        players: playerName
+        players: playerName,
     }
 
     selactionArea.push(playerObj);
@@ -45,9 +45,38 @@ function addToSelacted(element) {
 
 document.getElementById('btn-players-exp').addEventListener('click', function () {
     const totalSelected = document.getElementById('total-added-player');
+    // console.log(totalSelected);
     const totalSelectedString = totalSelected.innerText;
+    // console.log(totalSelectedString)
     const totalSelectedNumber = parseInt(totalSelectedString);
     const sum = totalSelectedNumber * 1000;
-    console.log(sum);
+    // console.log(sum);
+    const plrSum = document.getElementById('sub-total');
+    // console.log(plrSum)
+    plrSum.innerText = sum;
 
+})
+
+function getvalue(elementId) {
+    const eLement = document.getElementById(elementId);
+    const elementString = eLement.innerText;
+    const elementNumber = parseInt(elementString);
+    return elementNumber;
+}
+
+const managerCost = getvalue('manager-id');
+const coachCost = getvalue('coach-id');
+
+
+const playerCostvalue = document.getElementById('sub-total');
+const playerCostString = playerCostvalue.innerText;
+const playerCostNumber = parseInt(playerCostString);
+const playerCost = playerCostNumber;
+
+document.getElementById('net-expence-btn').addEventListener('click', function () {
+    const netExpence = playerCost + managerCost + coachCost;
+    console.log(managerCost)
+    console.log(coachCost)
+    console.log(playerCost)
+    console.log(netExpence);
 })
